4452dadcad714c0bbdf1f84ccce15b8614f89818
cringedev <vkiykouski@gmail.com>
2023-03-12 14:25:55 +0300

    added sql query

---
+++ query.sql
@@ -0,0 +1,28 @@
+SELECT Products.Name, Categories.Name
+FROM Products
+LEFT JOIN ProductCategories ON Products.ID = ProductCategories.ProductID
+LEFT JOIN Categories ON ProductCategories.CategoryID = Categories.ID
+
+
+-- --queries for preparing test data
+
+-- CREATE TABLE Products (
+--     Id INT PRIMARY KEY,
+--     Name NVARCHAR(50)
+-- );
+
+-- CREATE TABLE ProductCategories (
+--     ProductId INT,
+--     CategoryId INT,
+--     FOREIGN KEY (ProductId) REFERENCES Products(Id),
+--     FOREIGN KEY (CategoryId) REFERENCES Categories(Id)
+-- );
+
+-- INSERT INTO Categories (Id, Name)
+-- VALUES (1, 'Books'), (2, 'Electronics'), (3, 'Home Appliances');
+
+-- INSERT INTO Products (Id, Name)
+-- VALUES (1, 'Harry Potter'), (2, 'iPhone 14'), (3, 'MacBook Pro'), (4, 'Table'), (5, 'Xiaomi Mi Robot Vacuum'), (6, 'Snow');
+
+-- INSERT INTO ProductCategories (ProductId, CategoryId)
+-- VALUES (1, 1), (2, 2), (3, 2), (3, 3), (4, 3), (5, 2), (5, 3) ;
